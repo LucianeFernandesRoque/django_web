af7e4c3dcdfd642d35f195f911dd4e7cd812dbb7
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-02-14 20:22:58 -0300

    UPDATE FORM

---
+++ cadastro_curso_wo_makers/base/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class CadastroForm(forms.Form):
+    nome = forms.CharField()
+    email = forms.EmailField()
+    senha = forms.CharField(widget=forms.PasswordInput)
+   
