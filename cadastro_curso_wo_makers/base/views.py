d6daf55eb70eeae66a2250432c5d8a1c9db21be9
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-02-14 11:47:22 -0300

    Create template with html hard in routes

--- cadastro_curso_wo_makers/base/views.py
+++ cadastro_curso_wo_makers/base/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def inicio(request):
+    html = '''
+        <!Doctype html>
+        <head>
+            <title> Cursos WoMakersCode </title>
+        </head>
+        <body>
+            <h1> Hello world </h1>
+            <p> Primeiro projeto com django! </p>
+        </body>
+        </html>
+    '''
+    return HttpResponse(html)
+
+def cadastro(request):
+    pass
--- cadastro_curso_wo_makers/project_wo_makers/urls.py
+++ cadastro_curso_wo_makers/project_wo_makers/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from base.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inicio)
 ]
