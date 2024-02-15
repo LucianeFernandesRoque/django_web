cc4d6af90bfb121c61b67b1e9528da52c98a0a54
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-02-15 19:40:32 -0300

    Edit admin

--- cadastro_curso_wo_makers/base/forms.py
+++ cadastro_curso_wo_makers/base/forms.py
@@ -5,3 +5,4 @@ class CadastroForm(forms.ModelForm):
     class Meta:
         model = Cadastro
         fields = ['nome', 'email', 'senha']
+        widgets = {'senha': forms.PasswordInput()}
--- cadastro_curso_wo_makers/base/models.py
+++ cadastro_curso_wo_makers/base/models.py
@@ -7,3 +7,11 @@ class Cadastro(models.Model):
     email = models.EmailField(max_length=100)
     senha = models.CharField(max_length=50)
     data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.nome} - {self.email} - {self.data}'
+
+    class Meta:
+        verbose_name = 'Formulário de contato'
+        verbose_name_plural = 'Formulários de contato'
+        ordering = ['-data']
