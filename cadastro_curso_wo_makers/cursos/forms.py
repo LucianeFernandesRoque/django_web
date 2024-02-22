5df9959a347bae0d95e1560751c2106e7b9486e7
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-02-22 15:46:20 -0300

    Validate Fields

--- cadastro_curso_wo_makers/cursos/forms.py
+++ cadastro_curso_wo_makers/cursos/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from cursos.models import Curso
+from datetime import date
 
 class CursoForm(forms.ModelForm):
+    
+    def clean_data_do_curso(self):
+        data_do_curso = self.cleaned_data['data_do_curso']
+        hoje = date.today()
+        if data_do_curso < hoje:
+            raise forms.ValidationError('Não é possivel cadastrar um curso no passado')
+        return data_do_curso
     class Meta:
         model = Curso
         fields = ['titulo', 'nivel', 'carga_horaria', 'data_do_curso', 'descricao']
