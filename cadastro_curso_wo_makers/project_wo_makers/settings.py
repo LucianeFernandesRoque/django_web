0b9dcea93b966b33fd7ad3bca643356a1179b89f
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-02-29 19:41:43 -0300

    create url rest api

--- cadastro_curso_wo_makers/project_wo_makers/settings.py
+++ cadastro_curso_wo_makers/project_wo_makers/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
-    'bootstrap5',
+    'django_bootstrap5',
     'cursos',
     'rest_framework',
 ]
--- cadastro_curso_wo_makers/rest_api/views.py
+++ cadastro_curso_wo_makers/rest_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+# Ira implementar uma view onde é possivel obter essa mensagem de hello world api através de uma requisição get
+# e também enviar um nome para receber uma mensagem personalizada através do POST
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({'message': f'Hello, {request.data.get("name")}'})
+    return Response({'hello': 'WORLD API'})
