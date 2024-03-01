2bf267dbcd9f5177773a9581f3567f90984d4d69
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-03-01 09:56:32 -0300

    created serializer

--- cadastro_curso_wo_makers/rest_api/views.py
+++ cadastro_curso_wo_makers/rest_api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from cursos.models import Curso
+from rest_api.serializers import CursoModelSerializer
 # Create your views here.
 # Ira implementar uma view onde é possivel obter essa mensagem de hello world api através de uma requisição get
 # e também enviar um nome para receber uma mensagem personalizada através do POST
@@ -9,3 +12,7 @@ def hello_world(request):
     if request.method == 'POST':
         return Response({'message': f'Hello, {request.data.get("name")}'})
     return Response({'hello': 'WORLD API'})
+
+class CursoModelViewSet(ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoModelSerializer
