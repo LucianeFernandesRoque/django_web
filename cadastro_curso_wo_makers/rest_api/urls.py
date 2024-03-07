6030510d251cefced69712d44e459177d8e256bd
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-03-07 19:53:46 -0300

    include arch

--- cadastro_curso_wo_makers/rest_api/urls.py
+++ cadastro_curso_wo_makers/rest_api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_api.views import hello_world
 from rest_framework.routers import SimpleRouter
 from rest_api.views import CursoModelViewSet
+
 app_name = 'rest_api'
 router = SimpleRouter(trailing_slash=False)
 router.register('curso', CursoModelViewSet)
